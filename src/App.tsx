4520b597ddf3f490a20651788963d4b449883491
snailuu <67217277@qq.com>
2024-09-01 17:17:48 +0800

    fix: 头部布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import React from 'react';
+import ReactPlayground from './ReactPlayground';
+import './App.scss';
+function App() {
+	return <ReactPlayground />;
+}
 
-import iframeRaw from './iframe.html?raw';
-
-const iframeUrl = URL.createObjectURL(new Blob([iframeRaw], { type: 'text/html' }));
-
-const Preview: React.FC = () => {
-	return (
-		<iframe
-			src={iframeUrl}
-			style={{
-				width: '100%',
-				height: '100%',
-				padding: 0,
-				border: 'none',
-			}}
-		/>
-	);
-};
-
-export default Preview;
+export default App;
---
+++ src/ReactPlayground/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import logoSvg from '../icons/logo.svg';
+import styles from './index.module.scss';
+export default function Header() {
+	return (
+		<div className={styles.header}>
+			<div className={styles.logo}>
+				<img
+					src={logoSvg}
+					alt='logo'
+				/>
+				<span>React Playground</span>
+			</div>
+		</div>
+	);
+}
