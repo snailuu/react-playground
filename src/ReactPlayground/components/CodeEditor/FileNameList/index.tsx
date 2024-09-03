4e1d4e044b9a1ea54e427c61c013e2cfa988141b
snailuu <67217277@qq.com>
2024-09-03 22:38:10 +0800

    feat: 文件新增/删除

--- src/ReactPlayground/components/CodeEditor/FileNameList/index.tsx
+++ src/ReactPlayground/components/CodeEditor/FileNameList/index.tsx
@@ -3,6 +3,7 @@ import { PlaygroundContext } from '../../../PlaygroundContext';
 
 import { FileNameItem } from './FileNameItem';
 import styles from './index.module.scss';
+import { APP_COMPONENT_FILE_NAME, ENTRY_FILE_NAME, IMPORT_MAP_FILE_NAME } from '../../../files';
 
 export default function FileNameList() {
 	const { files, removeFile, addFile, updateFileName, selectedFileName, setSelectedFileName } =
@@ -14,18 +15,69 @@ export default function FileNameList() {
 		setTabs(Object.keys(files));
 	}, [files]);
 
+	const handleEditComplete = (name: string, prevName: string) => {
+		updateFileName(prevName, name);
+		setSelectedFileName(name);
+
+		setCreating(false);
+	};
+
+	const [creating, setCreating] = useState(false);
+
+	const addTab = () => {
+		const newFileName = 'Comp' + Math.random().toString().slice(2, 8) + '.tsx';
+		addFile(newFileName);
+		setSelectedFileName(newFileName);
+		setCreating(true);
+	};
+
+	const handleRemove = (name: string) => {
+		removeFile(name);
+		setSelectedFileName(ENTRY_FILE_NAME);
+	};
+
+	const readonlyFileNames = [ENTRY_FILE_NAME, IMPORT_MAP_FILE_NAME, APP_COMPONENT_FILE_NAME];
+
+	useEffect(() => {
+		const container = document.querySelector(`.${styles.tabs}`) as HTMLElement;
+		const handleWheel = (e: WheelEvent) => {
+			console.log(e);
+			e.preventDefault();
+			container.scrollLeft += e.deltaY;
+		};
+
+		// 添加事件监听器
+		container?.addEventListener('wheel', handleWheel);
+
+		// 组件卸载时移除事件监听器
+		return () => {
+			container?.removeEventListener('wheel', handleWheel);
+		};
+	}, []);
+
 	return (
 		<div className={styles.tabs}>
-			{tabs.map((item, index) => (
+			{tabs.map((item, index, arr) => (
 				<FileNameItem
 					key={item + index}
 					value={item}
+					readonly={readonlyFileNames.includes(item)}
+					creating={creating && index === arr.length - 1}
 					actived={selectedFileName === item}
 					onClick={() => {
 						console.log('点击了', item);
 						setSelectedFileName(item);
+					}}
+					onEditComplete={(name: string) => handleEditComplete(name, item)}
+					onRemove={() => {
+						handleRemove(item);
 					}}></FileNameItem>
 			))}
+			<div
+				className={styles.add}
+				onClick={addTab}>
+				+
+			</div>
 		</div>
 	);
 }
