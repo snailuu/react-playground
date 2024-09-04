ea85cab78ed7b255705fc57c2ee537fbd93d6038
hzf <snailuu@foxmail.com>
2024-09-04 13:40:31 +0800

    feat: 新增分享和下载功能

--- src/ReactPlayground/components/Header/index.tsx
+++ src/ReactPlayground/components/Header/index.tsx
@@ -2,9 +2,13 @@ import { useContext } from 'react';
 import logoSvg from '../icons/logo.svg';
 import styles from './index.module.scss';
 import { PlaygroundContext } from '../../PlaygroundContext';
-import { MoonOutlined, SunOutlined } from '@ant-design/icons';
+import { DownloadOutlined, MoonOutlined, ShareAltOutlined, SunOutlined } from '@ant-design/icons';
+import copy from 'copy-to-clipboard'
+import { message } from 'antd';
+import { downloadFiles } from '../../utils';
+
 export default function Header() {
-	const { theme, setTheme } = useContext(PlaygroundContext);
+	const { theme, setTheme, files } = useContext(PlaygroundContext);
 
 	return (
 		<div className={styles.header}>
@@ -30,6 +34,21 @@ export default function Header() {
 						onClick={() => setTheme('light')}
 					/>
 				)}
+				<ShareAltOutlined
+					style={{ marginLeft: '10px' }}
+					onClick={() => {
+						copy(window.location.href);
+						message.success('分享链接已复制')
+					}}
+				/>
+				<DownloadOutlined
+					style={{ marginLeft: '10px' }}
+					onClick={async () => {
+						await downloadFiles(files);
+						message.success('下载完成')
+					}}
+				/>
+
 			</div>
 		</div>
 	);
